1660ba13081ae84ff6a3d3edb3d17854b575448c
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-07-09 20:42:13 +0530

    clean up comments and import

--- online_store/test/template.dart
+++ online_store/test/template.dart
@@ -1,16 +1,9 @@
-// ignore_for_file: avoid_print test requires print
-
-import 'package:cl_basic_types/cl_basic_types.dart';
 import 'package:online_store/src/implementations/cl_server.dart';
 import 'package:test/test.dart';
 
 import 'test_modules.dart';
 import 'text_ext_on_cl_server.dart';
 import 'utils.dart';
-
-/// Add Tests for
-/// Creating a collection with file
-/// Creating a collection with parentId
 
 void main() async {
   late CLServer server;
