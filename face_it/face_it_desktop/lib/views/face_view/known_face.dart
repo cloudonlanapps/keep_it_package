ee027def599ec813dffdd721b835dce601271cf3
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-09-15 12:16:13 +0530

    fitted box for longer name

--- face_it/face_it_desktop/lib/views/face_view/known_face.dart
+++ face_it/face_it_desktop/lib/views/face_view/known_face.dart
@@ -16,58 +16,61 @@ class ConfirmedFace extends StatelessWidget {
       width: 320,
       height: 200,
       padding: const EdgeInsets.all(8),
-      child: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Center(
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                spacing: 8,
-                children: [
-                  Transform(
-                    transform: Matrix4.identity()
-                      ..setEntry(3, 2, 0.005) // Apply perspective
-                      ..rotateX(0.1) // Example: rotate along X-axis
-                      ..rotateY(-0.4), // Example: rotate along Y-axis
-                    alignment: FractionalOffset.center,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        FacePreview(face: face),
-                        Text('', style: ShadTheme.of(context).textTheme.h4),
-                      ],
+      child: FittedBox(
+        fit: BoxFit.cover,
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 8,
+                  children: [
+                    Transform(
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.005) // Apply perspective
+                        ..rotateX(0.1) // Example: rotate along X-axis
+                        ..rotateY(-0.4), // Example: rotate along Y-axis
+                      alignment: FractionalOffset.center,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          FacePreview(face: face),
+                          Text('', style: ShadTheme.of(context).textTheme.h4),
+                        ],
+                      ),
                     ),
-                  ),
-                  const Icon(LucideIcons.arrowBigRight300),
-                  Transform(
-                    transform: Matrix4.identity()
-                      ..setEntry(3, 2, 0.005) // Apply perspective
-                      ..rotateX(0.1) // Example: rotate along X-axis
-                      ..rotateY(0.4), // Example: rotate along Y-axis
-                    alignment: FractionalOffset.center,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        FacePreview(face: face),
-                        Text(
-                          face.registeredFace!.personName
-                              .capitalizeFirstLetter(),
-                          style: ShadTheme.of(context).textTheme.h4,
-                        ),
-                      ],
+                    const Icon(LucideIcons.arrowBigRight300),
+                    Transform(
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.005) // Apply perspective
+                        ..rotateX(0.1) // Example: rotate along X-axis
+                        ..rotateY(0.4), // Example: rotate along Y-axis
+                      alignment: FractionalOffset.center,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          FacePreview(face: face),
+                          Text(
+                            face.registeredFace!.personName
+                                .capitalizeFirstLetter(),
+                            style: ShadTheme.of(context).textTheme.h4,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          const Align(
-            alignment: Alignment.bottomRight,
-            child: Icon(LucideIcons.flag300),
-          ),
-        ],
+            const Align(
+              alignment: Alignment.bottomRight,
+              child: Icon(LucideIcons.flag300),
+            ),
+          ],
+        ),
       ),
     );
   }
