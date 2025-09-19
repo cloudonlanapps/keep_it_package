f68b1848b0ff1f3bea7f73e49606ad35d4e70a3d
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-09-19 08:30:19 +0530

    Info popOver for the chart

--- face_it/face_it_desktop/lib/views/server/views/upload_progress.dart
+++ face_it/face_it_desktop/lib/views/server/views/upload_progress.dart
@@ -2,15 +2,30 @@ import 'package:face_it_desktop/views/server/models/upload_status.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 import '../../../content_manager.dart/providers/candidates.dart';
 import '../providers/uploader.dart';
 
-class UploadProgressChart extends ConsumerWidget {
+class UploadProgressChart extends ConsumerStatefulWidget {
   const UploadProgressChart({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<UploadProgressChart> createState() =>
+      UploadProgressChartState();
+}
+
+class UploadProgressChartState extends ConsumerState<UploadProgressChart> {
+  final popoverController = ShadPopoverController();
+
+  @override
+  void dispose() {
+    popoverController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final uploader = ref
         .watch(uploaderProvider)
         .whenOrNull(data: (data) => data);
@@ -19,29 +34,68 @@ class UploadProgressChart extends ConsumerWidget {
     final candidates = ref.watch(candidatesProvider.select((e) => e.items));
     const radius = 20.0;
     const radius2 = 20.0;
-    return Center(
-      child: AspectRatio(
-        aspectRatio: 1,
-        child: PieChart(
-          PieChartData(
-            sections: [
-              for (final status in UploadStatus.values)
-                PieChartSectionData(
-                  value: uploader.countByStatus(status).toDouble(),
-                  color: colors(status),
-                  radius: radius,
-                  showTitle: false,
-                  title: status.name,
+
+    return ShadPopover(
+      controller: popoverController,
+      popover: (context) {
+        return ShadCard(
+          width: 120,
+          padding: EdgeInsets.zero,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 8,
+            children: UploadStatus.values.map((e) {
+              return Row(
+                spacing: 8,
+                children: [
+                  Container(
+                    width: 16,
+                    height: 16,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      color: colors(e),
+                    ),
+                  ),
+                  Text(e.name, style: ShadTheme.of(context).textTheme.small),
+                ],
+              );
+            }).toList(),
+          ),
+        );
+      },
+      child: GestureDetector(
+        onTap: popoverController.toggle,
+        child: Center(
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Stack(
+              children: [
+                const Center(child: Icon(LucideIcons.info400)),
+                PieChart(
+                  PieChartData(
+                    sections: [
+                      for (final status in UploadStatus.values)
+                        PieChartSectionData(
+                          value: uploader.countByStatus(status).toDouble(),
+                          color: colors(status),
+                          radius: radius,
+                          showTitle: false,
+                          title: status.name,
+                        ),
+                      PieChartSectionData(
+                        value: (candidates.length - uploader.count).toDouble(),
+                        color: Colors.grey,
+                        radius: radius,
+                        showTitle: false,
+                      ),
+                    ],
+                    sectionsSpace: 0,
+                    centerSpaceRadius: radius2, // This creates the donut shape
+                  ),
                 ),
-              PieChartSectionData(
-                value: (candidates.length - uploader.count).toDouble(),
-                color: Colors.grey,
-                radius: radius,
-                showTitle: false,
-              ),
-            ],
-            sectionsSpace: 0,
-            centerSpaceRadius: radius2, // This creates the donut shape
+              ],
+            ),
           ),
         ),
       ),
