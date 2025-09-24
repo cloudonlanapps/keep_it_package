048e891f94fe2ab2897aa279f812f3933ec7737d
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-09-24 08:37:31 +0530

    try color coding

--- cl_basic_types/lib/src/cl_logger.dart
+++ cl_basic_types/lib/src/cl_logger.dart
@@ -1,5 +1,16 @@
 import 'dart:developer' as dev;
 
+class TColor {
+  static const red = "\x1b[31m";
+  static const green = "\x1b[32m";
+  static const yellow = "\x1b[33m";
+  static const blue = "\x1b[34m";
+  static const reset = "\x1b[0m";
+
+  static const bold = "\x1b[1m";
+  static const underline = "\x1b[4m";
+}
+
 mixin CLLogger {
   String get logPrefix;
   void log(
@@ -9,11 +20,11 @@ mixin CLLogger {
     StackTrace? stackTrace,
   }) {
     dev.log(
-      message,
+      "${TColor.blue}$message${TColor.reset}",
       level: level,
       error: error,
       stackTrace: stackTrace,
-      name: logPrefix,
+      name: "${TColor.bold}${TColor.blue}$logPrefix${TColor.reset}",
     );
   }
 }
