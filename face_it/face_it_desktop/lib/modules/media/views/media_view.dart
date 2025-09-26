6e4cdb2ce250b3c9e5441b3897c0a4f4cc9181f6
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-09-26 15:33:22 +0530

    show face layer

--- face_it/face_it_desktop/lib/modules/media/views/media_view.dart
+++ face_it/face_it_desktop/lib/modules/media/views/media_view.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
+import '../../face_recg/providers/face_recg.dart';
+import '../../face_recg/views/face_layer.dart';
+import '../../settings/providers/face_box_preferences.dart';
 import '../../settings/views/quick_settings.dart';
 import '../providers/candidates.dart';
 import 'media.dart';
@@ -19,6 +22,22 @@ class _ContentViewerState extends ConsumerState<MediaViewer> {
     final activeCandidate = ref.watch(
       mediaListProvider.select((candidates) => candidates.activeCandidate),
     );
+
+    final bool showFaceBoxes;
+    final List<String>? faces;
+    if (activeCandidate != null) {
+      faces = ref.watch(
+        faceRecgProvider.select(
+          (e) => e.getFaceIds(activeCandidate.path) ?? [],
+        ),
+      );
+      showFaceBoxes =
+          ref.watch(faceBoxPreferenceProvider.select((e) => e.enabled)) &&
+          faces != null;
+    } else {
+      showFaceBoxes = false;
+      faces = null;
+    }
 
     return Column(
       children: [
@@ -46,6 +65,10 @@ class _ContentViewerState extends ConsumerState<MediaViewer> {
                               alignment: Alignment.topLeft,
                               child: Media(filePath: activeCandidate.path),
                             ),
+                            if (showFaceBoxes)
+                              Positioned.fill(
+                                child: FaceLayer(faceIds: faces!),
+                              ),
                           ],
                         ),
                       ),
