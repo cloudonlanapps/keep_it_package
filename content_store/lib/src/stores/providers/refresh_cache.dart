7b806d94c10b30ff99db11ccafc403629587bf3c
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-10-16 13:12:34 +0530

    Moving to Notifier for ReloadNotifier

--- content_store/lib/src/stores/providers/refresh_cache.dart
+++ content_store/lib/src/stores/providers/refresh_cache.dart
@@ -2,21 +2,26 @@ import 'dart:developer';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ReloadNotifier extends StateNotifier<String> {
-  ReloadNotifier() : super(DateTime.now().toIso8601String());
+class ReloadNotifier extends Notifier<String> {
+  @override
+  String build() {
+    listenSelf((prev, curr) {
+      log(
+        'Trigger Refresh at $curr',
+        name: 'refreshReaderProvider',
+        time: DateTime.now(),
+      );
+    });
+
+    // Return the initial state
+    return DateTime.now().toIso8601String();
+  }
 
   void reload() {
     state = DateTime.now().toIso8601String();
   }
 }
 
-final reloadProvider = StateNotifierProvider<ReloadNotifier, String>((ref) {
-  ref.listenSelf((prev, curr) {
-    log(
-      'Trigger Refresh at $curr',
-      name: 'refreshReaderProvider',
-      time: DateTime.now(),
-    );
-  });
-  return ReloadNotifier();
-});
+final reloadProvider = NotifierProvider<ReloadNotifier, String>(
+  ReloadNotifier.new,
+);
