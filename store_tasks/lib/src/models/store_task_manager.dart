754abaa5f6cb712f14772086c04c5fb8fe71db3f
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2026-02-16 23:34:22 +0530

    delete all files moved in refactoring till now

--- store_tasks/lib/src/models/store_task_manager.dart
+++
@@ -1,6 +0,0 @@
-import 'store_task.dart';
-
-abstract class StoreTaskManager {
-  bool add(StoreTask task);
-  StoreTask? pop();
-}
--- store_tasks/lib/src/providers/target_store_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:store/store.dart';
-
-final targetStoreProvider = StateProvider<CLStore>((ref) {
-  throw Exception('Must be overridden');
-});
