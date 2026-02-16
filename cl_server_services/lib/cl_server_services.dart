a12ebe39400454639e9e0e89bd61a121c77d9550
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2026-02-16 12:07:13 +0530

    refactor: move network service related files to `src/network_services` directory

--- cl_server_services/lib/cl_server_services.dart
+++ cl_server_services/lib/cl_server_services.dart
@@ -9,15 +9,25 @@ export 'package:cl_server_dart_client/cl_server_dart_client.dart'
         ServerHealthStatus,
         ServerPreferences;
 
-export 'src/builders/get_active_a_i_server.dart' show GetActiveAIServer;
-export 'src/builders/get_available_servers.dart' show GetAvailableServers;
-export 'src/builders/get_nw_scanner.dart' show GetNetworkScanner;
-export 'src/builders/get_server_session.dart' show GetServerSession;
-export 'src/providers/active_ai_server.dart' show activeAIServerProvider;
-export 'src/providers/available_servers.dart' show availableServersProvider;
-export 'src/providers/network_scanner.dart' show networkScannerProvider;
-export 'src/providers/server_health_check.dart' show serverHealthCheckProvider;
-export 'src/providers/server_preference.dart' show serverPreferenceProvider;
-export 'src/providers/server_provider.dart' show serverProvider;
-export 'src/providers/socket_connection.dart'
+export 'src/network_services/builders/get_active_a_i_server.dart'
+    show GetActiveAIServer;
+export 'src/network_services/builders/get_available_servers.dart'
+    show GetAvailableServers;
+export 'src/network_services/builders/get_nw_scanner.dart'
+    show GetNetworkScanner;
+export 'src/network_services/builders/get_server_session.dart'
+    show GetServerSession;
+export 'src/network_services/providers/active_ai_server.dart'
+    show activeAIServerProvider;
+export 'src/network_services/providers/available_servers.dart'
+    show availableServersProvider;
+export 'src/network_services/providers/network_scanner.dart'
+    show networkScannerProvider;
+export 'src/network_services/providers/server_health_check.dart'
+    show serverHealthCheckProvider;
+export 'src/network_services/providers/server_preference.dart'
+    show serverPreferenceProvider;
+export 'src/network_services/providers/server_provider.dart'
+    show serverProvider;
+export 'src/network_services/providers/socket_connection.dart'
     show SocketConnectionNotifier, socketConnectionProvider;
